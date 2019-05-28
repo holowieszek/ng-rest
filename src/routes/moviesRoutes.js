e6ef752ed7a81ee496fe1bf16a548779ad0dccc1
Mateusz <holowieszko.m@gmail.com>
2019-05-28 22:09:45 +0200

    Added middleware to /movies POST endpoint for connecting with OMDB API

--- src/routes/moviesRoutes.js
+++ src/routes/moviesRoutes.js
@@ -1,11 +1,12 @@
 import { Router } from 'express';
 
 import * as moviesController from '../controllers/movies';
+import omdbApi from '../middlewares/omdbApi';
 
 const router = Router();
 
 router.get('/', moviesController.fetchAll);
 
-router.post('/', moviesController.create);
+router.post('/', omdbApi, moviesController.create);
 
 export default router;
