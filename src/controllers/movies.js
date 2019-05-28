84c01b299053fc1320031395704afd7ddd8351bf
Mateusz <holowieszko.m@gmail.com>
2019-05-28 14:21:47 +0200

    Changed way of default responses on helper method

--- src/controllers/movies.js
+++ src/controllers/movies.js
@@ -1,17 +1,18 @@
 import HttpStatus from 'http-status-codes';
 
+import { returnResponse } from '../utils/response';
 import * as moviesService from '../services/moviesServices';
 
 export function fetchAll(req, res, next) {
   return moviesService
     .fetchAllMovies()
-    .then(data => res.status(HttpStatus.OK).json({ data }))
+    .then(data => returnResponse(res, HttpStatus.OK, data))
     .catch(err => next(err));
 }
 
 export function create(req, res, next) {
   moviesService
     .createMovie()
-    .then(data => res.status(HttpStatus.CREATED).json({ data }))
+    .then(data => returnResponse(res, HttpStatus.CREATED, data))
     .catch(err => next(err));
 }
