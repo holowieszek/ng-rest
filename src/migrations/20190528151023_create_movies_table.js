5a83d4b065976b7451932f7ec1725965cfed3e93
Mateusz <holowieszko.m@gmail.com>
2019-05-28 23:26:34 +0200

    Added primary key

--- src/migrations/20190528151023_create_movies_table.js
+++ src/migrations/20190528151023_create_movies_table.js
@@ -1,6 +1,6 @@
 export async function up(knex) {
   await knex.schema.createTable('movies', table => {
-    table.increments();
+    table.increments('id').primary();
     table.string('title');
     table.string('year');
     table.string('rated');
