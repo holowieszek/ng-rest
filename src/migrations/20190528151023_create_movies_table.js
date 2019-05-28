09c25e4662ad85bc291ee84c67f7b3498894ad71
Mateusz <holowieszko.m@gmail.com>
2019-05-28 22:08:53 +0200

    Added tables for handling data from API

--- src/migrations/20190528151023_create_movies_table.js
+++ src/migrations/20190528151023_create_movies_table.js
@@ -1,7 +1,29 @@
 export async function up(knex) {
   await knex.schema.createTable('movies', table => {
     table.increments();
-    table.string('name');
+    table.string('title');
+    table.string('year');
+    table.string('rated');
+    table.string('released');
+    table.string('runtime');
+    table.string('genre');
+    table.string('director');
+    table.string('writer');
+    table.string('actors');
+    table.string('plot');
+    table.string('language');
+    table.string('country');
+    table.string('awards');
+    table.string('poster');
+    table.string('metascore');
+    table.string('imdbrating');
+    table.string('imdbvotes');
+    table.string('imdbid');
+    table.string('type');
+    table.string('dvd');
+    table.string('boxoffice');
+    table.string('production');
+    table.string('website');
     table.timestamps();
   });
 }
