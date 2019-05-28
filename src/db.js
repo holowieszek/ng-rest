7b35841a2b25ad0bac42db140387c2348b598a76
Mateusz <holowieszko.m@gmail.com>
2019-05-28 15:13:51 +0200

    Added DB configuration files

---
+++ src/db.js
@@ -0,0 +1,12 @@
+import knexJS from 'knex';
+import bookshelfJS from 'bookshelf';
+
+import knexConfig from './knexconfig';
+
+// Connection
+const knex = knexJS(knexConfig);
+const bookshelf = bookshelfJS(knex);
+
+bookshelf.plugin(['virtuals', 'pagination', 'visibility']);
+
+export default bookshelf;
