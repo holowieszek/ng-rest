fa8a44c9da8bb8f9a97dab431dad78cb6b70cefe
Mateusz <holowieszko.m@gmail.com>
2019-05-28 22:11:29 +0200

    Added method for saving data in db

--- src/services/moviesServices.js
+++ src/services/moviesServices.js
@@ -4,6 +4,18 @@ export function fetchAllMovies() {
   return Movie.fetchAll();
 }
 
-export function createMovie(name) {
-  return Movie.forge({ name }).save();
+export function createMovie(movie) {
+  const { data } = movie;
+  let movieInformations = {}
+
+  for (var key in data) {
+    if (data.hasOwnProperty(key)) {
+      let keyToLowerCase = key.toLowerCase();
+      movieInformations[keyToLowerCase] = data[key];
+    }
+  }
+
+  const { ratings, response, ...parsedData } = movieInformations;
+
+  return Movie.forge(parsedData).save();
 }
