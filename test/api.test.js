9071fc92169b50535e97912dc7c2a10454d59cd0
Mateusz <holowieszko.m@gmail.com>
2019-05-29 18:18:31 +0200

    API tests

---
+++ test/api.test.js
@@ -0,0 +1,18 @@
+import { expect } from 'chai';
+import app from '../src/index';
+import request from 'supertest';
+
+describe('Base API Test', () => {
+  it('should return API version and title', done => {
+    request(app)
+      .get('/api')
+      .end((err, res) => {
+        const { title, version } = res.body;
+
+        expect(res.statusCode).to.be.equal(200);
+        expect(title).to.be.equal(app.locals.title);
+        expect(version).to.be.equal(app.locals.version);
+        done();
+      });
+  });
+});
