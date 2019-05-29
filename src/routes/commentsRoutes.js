e92c62bd30d79bf72d1917dbd68ecd804aca4cd0
Mateusz <holowieszko.m@gmail.com>
2019-05-29 14:35:42 +0200

    Added validation middleware for POST method in comments & movies routes

--- src/routes/commentsRoutes.js
+++ src/routes/commentsRoutes.js
@@ -1,9 +1,10 @@
 import { Router } from 'express';
 import * as commentsController from '../controllers/comments';
+import { commentValidator } from '../validators/commentsValidator';
 
 const router = Router();
 
 router.get('/', commentsController.fetchAll);
-router.post('/', commentsController.create);
+router.post('/', commentValidator, commentsController.create);
 
 export default router;
--- src/routes/moviesRoutes.js
+++ src/routes/moviesRoutes.js
@@ -2,10 +2,11 @@ import { Router } from 'express';
 
 import * as moviesController from '../controllers/movies';
 import omdbApi from '../middlewares/omdbApi';
+import { moviesValidator } from '../validators/moviesValidator';
 
 const router = Router();
 
 router.get('/', moviesController.fetchAll);
-router.post('/', omdbApi, moviesController.create);
+router.post('/', moviesValidator, omdbApi, moviesController.create);
 
 export default router;
