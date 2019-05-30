bad04cbb664b1de80cbfce62ad097077d9685773
Mateusz <holowieszko.m@gmail.com>
2019-05-30 16:07:20 +0200

    Added pagination validator

---
+++ src/validators/paginationValidator.js
@@ -0,0 +1,19 @@
+import Joi from 'joi';
+
+import validate from '../utils/validate';
+import asyncWrapper from '../utils/asyncWrapper';
+
+const SCHEMA = {
+  page: Joi.number().positive(),
+  limit: Joi.number().positive()
+};
+
+async function paginationValidator(req, res, next) {
+  const { query } = req;
+
+  const { error } = await asyncWrapper(validate(query, SCHEMA));
+
+  error ? next(error) : next();
+}
+
+export { paginationValidator };
