74b6e1bb287a0aa0482e1220354f673368282224
Mateusz <holowieszko.m@gmail.com>
2019-05-30 12:21:14 +0200

    Swagger

--- dist/index.js
+++ dist/index.js
@@ -9,31 +9,42 @@ require("./env");
 
 require("./db");
 
-var _express = _interopRequireDefault(require("express"));
+const _express = _interopRequireDefault(require("express"));
 
-var _morgan = _interopRequireDefault(require("morgan"));
+const _morgan = _interopRequireDefault(require("morgan"));
 
-var _cors = _interopRequireDefault(require("cors"));
+const _cors = _interopRequireDefault(require("cors"));
 
-var _helmet = _interopRequireDefault(require("helmet"));
+const _helmet = _interopRequireDefault(require("helmet"));
 
-var _compression = _interopRequireDefault(require("compression"));
+const _compression = _interopRequireDefault(require("compression"));
 
-var _bodyParser = _interopRequireDefault(require("body-parser"));
+const _bodyParser = _interopRequireDefault(require("body-parser"));
 
-var _logger = _interopRequireWildcard(require("./utils/logger"));
+const _logger = _interopRequireWildcard(require("./utils/logger"));
 
-var errorHandler = _interopRequireWildcard(require("./middlewares/errorHandler"));
+const errorHandler = _interopRequireWildcard(require("./middlewares/errorHandler"));
 
-var _routes = _interopRequireDefault(require("./routes"));
+const _routes = _interopRequireDefault(require("./routes"));
 
-function _interopRequireWildcard(obj) { if (obj && obj.__esModule) { return obj; } else { var newObj = {}; if (obj != null) { for (var key in obj) { if (Object.prototype.hasOwnProperty.call(obj, key)) { var desc = Object.defineProperty && Object.getOwnPropertyDescriptor ? Object.getOwnPropertyDescriptor(obj, key) : {}; if (desc.get || desc.set) { Object.defineProperty(newObj, key, desc); } else { newObj[key] = obj[key]; } } } } newObj.default = obj; return newObj; } }
+const _swaggerUiExpress = _interopRequireDefault(require("swagger-ui-express"));
+
+const _swagger = _interopRequireDefault(require("./swagger.json"));
+
+function _interopRequireWildcard(obj) { if (obj && obj.__esModule) { return obj; } else { const newObj = {};
+
+ if (obj != null) { for (const key in obj) { if (Object.prototype.hasOwnProperty.call(obj, key)) { const desc = Object.defineProperty && Object.getOwnPropertyDescriptor ? Object.getOwnPropertyDescriptor(obj, key) : {};
+
+ if (desc.get || desc.set) { Object.defineProperty(newObj, key, desc); } else { newObj[key] = obj[key]; } } } } newObj.default = obj; 
+
+return newObj; } }
 
 function _interopRequireDefault(obj) { return obj && obj.__esModule ? obj : { default: obj }; }
 
 const app = (0, _express.default)();
 const APP_PORT = (process.env.NODE_ENV === 'test' ? process.env.TEST_APP_PORT : process.env.APP_PORT) || process.env.PORT || '3000';
 const APP_HOST = process.env.APP_HOST || '0.0.0.0';
+
 app.set('port', APP_PORT);
 app.set('host', APP_HOST);
 app.locals.title = process.env.APP_NAME;
@@ -47,7 +58,8 @@ app.use((0, _morgan.default)('tiny', {
 }));
 app.use(errorHandler.bodyParser); // Routing
 
-app.use('/api', _routes.default); // Error Handlers
+app.use('/api', _routes.default);
+app.use('/api-docs', _swaggerUiExpress.default.serve, _swaggerUiExpress.default.setup(_swagger.default)); // Error Handlers
 
 app.use(errorHandler.notFound); // app.use(errorHandler.methodNotAllowed);
 
@@ -59,5 +71,6 @@ app.listen(app.get('port'), app.get('host'), () => {
 process.on('unhandledRejection', err => {
   _logger.default.error('Unhandled rejection', err);
 });
-var _default = app;
+const _default = app;
+
 exports.default = _default;
--- dist/routes.js
+++ dist/routes.js
@@ -5,24 +5,31 @@ Object.defineProperty(exports, "__esModule", {
 });
 exports.default = void 0;
 
-var _express = require("express");
+const _express = require("express");
 
-var _httpStatusCodes = _interopRequireDefault(require("http-status-codes"));
+const _httpStatusCodes = _interopRequireDefault(require("http-status-codes"));
 
-var _moviesRoutes = _interopRequireDefault(require("./routes/moviesRoutes"));
+const _moviesRoutes = _interopRequireDefault(require("./routes/moviesRoutes"));
 
-var _commentsRoutes = _interopRequireDefault(require("./routes/commentsRoutes"));
+const _commentsRoutes = _interopRequireDefault(require("./routes/commentsRoutes"));
 
 function _interopRequireDefault(obj) { return obj && obj.__esModule ? obj : { default: obj }; }
 
 const router = (0, _express.Router)();
+
 router.get('/', (req, res) => {
+  const {
+    title,
+    version
+  } = req.app.locals;
+
   res.status(_httpStatusCodes.default.OK).json({
-    title: req.app.locals.title,
-    version: req.app.locals.version
+    title,
+    version
   });
 });
 router.use('/movies', _moviesRoutes.default);
 router.use('/comments', _commentsRoutes.default);
-var _default = router;
+const _default = router;
+
 exports.default = _default;
