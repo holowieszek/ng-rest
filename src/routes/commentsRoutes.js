9eeedd64590a5aa8dbc23ee2b08c630db4a1dd9b
Mateusz <holowieszko.m@gmail.com>
2019-05-30 16:04:45 +0200

    Added paginationValidator as middleware

--- src/routes/commentsRoutes.js
+++ src/routes/commentsRoutes.js
@@ -1,10 +1,11 @@
 import { Router } from 'express';
 import * as commentsController from '../controllers/comments';
 import { commentValidator } from '../validators/commentValidator';
+import { paginationValidator } from '../validators/paginationValidator';
 
 const router = Router();
 
-router.get('/', commentsController.fetchAll);
+router.get('/', paginationValidator, commentsController.fetchAll);
 router.post('/', commentValidator, commentsController.create);
 
 export default router;
--- src/routes/moviesRoutes.js
+++ src/routes/moviesRoutes.js
@@ -3,10 +3,11 @@ import { Router } from 'express';
 import * as moviesController from '../controllers/movies';
 import omdbApi from '../middlewares/omdbApi';
 import { movieValidator } from '../validators/movieValidator';
+import { paginationValidator } from '../validators/paginationValidator';
 
 const router = Router();
 
-router.get('/', moviesController.fetchAll);
+router.get('/', paginationValidator, moviesController.fetchAll);
 router.post('/', movieValidator, omdbApi, moviesController.create);
 
 export default router;
